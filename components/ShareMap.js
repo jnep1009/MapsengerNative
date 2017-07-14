d7f3c012745ea2a41d38a6d61f5e0a998f9df4f5
jnep1009 <junep1009@gmail.com>
2017-07-14 14:04:37 -0700

    Added callout

--- components/ShareMap.js
+++ components/ShareMap.js
@@ -1,171 +1,9 @@
 /**  * Created by JNEP on 7/7/17.  */
 import React, { Component, PropTypes } from 'react';
-import { View, AppRegistry, Dimensions, Image } from 'react-native';
+import { View, AppRegistry, Dimensions, Image, Text, StyleSheet } from 'react-native';
 //import { GradientView } from '../../Common'
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 const { width, height } = Dimensions.get('window');
-const mapStyle = [
-    {
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#242f3e"
-            }
-        ]
-    },
-    {
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#746855"
-            }
-        ]
-    },
-    {
-        "elementType": "labels.text.stroke",
-        "stylers": [
-            {
-                "color": "#242f3e"
-            }
-        ]
-    },
-    {
-        "featureType": "administrative.locality",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#d59563"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#d59563"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.park",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#263c3f"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.park",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#6b9a76"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#38414e"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "geometry.stroke",
-        "stylers": [
-            {
-                "color": "#212a37"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#9ca5b3"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#746855"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "geometry.stroke",
-        "stylers": [
-            {
-                "color": "#1f2835"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#f3d19c"
-            }
-        ]
-    },
-    {
-        "featureType": "transit",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#2f3948"
-            }
-        ]
-    },
-    {
-        "featureType": "transit.station",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#d59563"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#17263c"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#515c6d"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "labels.text.stroke",
-        "stylers": [
-            {
-                "color": "#17263c"
-            }
-        ]
-    }
-];
-
 
 export class ShareMap extends Component {
     constructor(props) {
@@ -173,12 +11,14 @@ export class ShareMap extends Component {
         this.state = {};
     }
 
+    show() {
+        this.marker1.showCallout();
+    }
+
     render() {
         return (
             <View style={{ position: 'relative', height: height}}>
                 <MapView
-                    provider={MapView.PROVIDER_GOOGLE}
-                    customMapStyle={mapStyle}
                     style={{ left:0, right: 0, top:0, bottom: 0, position: 'absolute' }}
                     initialRegion={{
             latitude: 37.78825,
@@ -187,20 +27,37 @@ export class ShareMap extends Component {
             longitudeDelta: 0.0421,
           }}>
                     <MapView.Marker
+                        ref={ref => { this.marker1 = ref; }}
+                        onPress={() => this.show()}
                         coordinate={{
-                            latitude: 37.78825,
+                           latitude: 37.78825,
                            longitude: -122.4324
                         }}>
                         <Image
                             style={{width: 40, height: 40, borderRadius: 40 / 2, borderWidth: 1, borderColor: 'white'}}
                             source={{uri: this.props.user.avatarUrl}}/>
-                        </MapView.Marker>
+                        <MapView.Callout
+                            style={styles.plainView}
+                        >
+                            <View
+                            >
+                                <Text>This is a plain view</Text>
+                            </View>
+                        </MapView.Callout>
+                    </MapView.Marker>
                 </MapView>
             </View>
-        )
-            ;
+        );
     }
 }
+
+const styles = StyleSheet.create({
+    plainView: {
+        width: 60,
+    }
+});
+
+
 ShareMap.propTypes = {
     currentLoc: PropTypes.array,
     user: PropTypes.object
